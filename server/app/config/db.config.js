6d3779d6258ec08c3752bfdcaa036593f681d5cc
daria <pechenaya.daria@gmail.com>
2022-11-30 22:03:50 +0400

    add heroku db

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,8 +1,8 @@
 const dbConfig = {
-	HOST: "localhost",
-	USER: "root",
-	PASSWORD: "1234",
-	DB: "04_admin_tool_bd",
+	HOST: "eu-cdbr-west-03.cleardb.net",
+	USER: "b9ac5fe09ec6ae",
+	PASSWORD: "0620796e",
+	DB: "heroku_5de42b046ce2b4e",
 	dialect: "mysql",
 	pool: {
 		max: 5,
@@ -13,3 +13,5 @@ const dbConfig = {
 };
 
 export default dbConfig;
+
+//mysql:\b9ac5fe09ec6ae:0620796e@eu-cdbr-west-03.cleardb.net\heroku_5de42b046ce2b4e?reconnect=true
